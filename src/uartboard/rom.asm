57bb525895f60f38d2139626ba83876d480ab5b3
ifilot <ivo@ivofilot.nl>
2025-06-05 19:41:54 +0200

    Rewriting test rom and adding GA

--- src/uartboard/rom.asm
+++ src/uartboard/rom.asm
@@ -1,88 +1,61 @@
 bits 16
-
-%define UART_BASE      0x20
-
-%define UART_THR       UART_BASE + 0    ; Transmit Holding Register (write)
-%define UART_RBR       UART_BASE + 0    ; Receiver Buffer Register (read)
-%define UART_DLL       UART_BASE + 0    ; Divisor Latch LSB (when DLAB=1)
-
-%define UART_IER       UART_BASE + 1    ; Interrupt Enable Register
-%define UART_DLM       UART_BASE + 1    ; Divisor Latch MSB (when DLAB=1)
-
-%define UART_IIR       UART_BASE + 2    ; Interrupt Identification Register (read)
-%define UART_FCR       UART_BASE + 2    ; FIFO Control Register (write)
-
-%define UART_LCR       UART_BASE + 3    ; Line Control Register
-%define UART_MCR       UART_BASE + 4    ; Modem Control Register
-%define UART_LSR       UART_BASE + 5    ; Line Status Register
-%define UART_MSR       UART_BASE + 6    ; Modem Status Register
-%define UART_SCR       UART_BASE + 7    ; Scratch Register (optional)
-
-; === Bit definitions
-%define LCR_DLAB       0x80
-%define LCR_8N1        0x03
-%define FCR_ENABLE     0x01
-%define FCR_CLEAR_RX   0x02
-%define FCR_CLEAR_TX   0x04
-%define MCR_RTS        0x02
-%define MCR_DTR        0x01
-%define MCR_OUT2       0x08
-%define LSR_THRE       0x20     ; Transmit Holding Register Empty
-%define MSR_CTS        0x10     ; Clear To Send
-
-%define LF             0x0A     ; Line Feed
-%define CR             0x0D     ; Carriage Return
-
-; === This code runs at 0x80000 physical address
 org 0x80000
 
-; position stack
-mov ax, 0x1000
-mov ss, ax
-mov sp, 0xFFFE
+; include constants for UART, characters, etc.
+%include "constants.inc"
 
-; initialize uart
-call init_uart
+;------------------------------------------------------------------------------
+; boot routine
+;------------------------------------------------------------------------------
+boot:
+    ; set-up stack
+    mov ax, 0x1000
+    mov ss, ax
+    mov sp, 0xFFFE
 
-; test
-mov al, 0xAA
-call puthex
-call newline
+    ; initialize system
+    call init
 
-; set-up ISR
-mov ax,0
-mov es, ax
-mov ax, isr_uart
-mov word [es:0x60 * 4], ax          ; write offset
-mov ax, 0x8000                      ; write segment
-mov [es:0x60 * 4 + 2], ax
+    ; show '@'
+    mov al, '@'
+    call putch
 
-mov ax, [es:0x60 * 4]
-call puthex16
-call newline
-
-mov ax, [es:0x60 * 4 + 2]
-call puthex16
-call newline
-
-; Enable interrupts
-sti
-
-; send "ready" char
-mov al, '@'
-call putch
-
-int 0x60
+    ; go to loop routine
+    jmp loop
 
 loop:
-    mov al, 0xAA
-    out 0x00, al
-    call delay
-    mov al, 0x55
-    out 0x00, al
-    call delay
+    mov al, [KEYBUFRPTR]
+    mov ah, [KEYBUFLPTR]
+    cmp ah, al
+    je loop
+    mov si, KEYBUF
+    xor bx, bx
+    mov bl, [KEYBUFLPTR]
+    add si, bx
+    mov al, [si]
+    inc bl
+    mov [KEYBUFLPTR], bl
+    call putch
+    mov al, bl
+    out LEDBASE, al
+    
+    jmp loop
 
-jmp loop
+;------------------------------------------------------------------------------
+; Initialize system
+;------------------------------------------------------------------------------
+init:
+    call init_uart      ; universal asynchronous serial communication
+    call init_ivt       ; interrupt vector table
+
+    mov al, 0x00        ; ensure LEDs are off
+    out 0x00, al
+
+    sti                 ; enable interrupts
+
+    xor ax, ax          ; reset AX
+    mov ds, ax          ; reset DS
+    ret
 
 ;
 ; 500 ms delay function for the NEC V20, about 
@@ -101,24 +74,9 @@ delay:
     loop .outer_loop   ; ~17 cycles when taken
     ret
 
-;
-; 500 ms delay function for the NEC V20, about 
-;
-delay_long:
-    mov cx, 2000       ; Outer loop × inner loop = total delay
-.outer_loop:
-    mov dx, 250       ; Inner loop count (fine-tuned)
-.inner_loop:
-    nop               ; 1 cycle
-    nop               ; 1 cycle
-    nop               ; 1 cycle
-    nop               ; 1 cycle
-    dec dx            ; ~3 cycles
-    jnz .inner_loop    ; ~16 cycles when taken
-    loop .outer_loop   ; ~17 cycles when taken
-    ret
-
+;------------------------------------------------------------------------------
 ; initialize the UART
+;------------------------------------------------------------------------------
 init_uart:
     ; Enable DLAB to access divisor registers
     mov al, LCR_DLAB
@@ -146,10 +104,29 @@ init_uart:
     mov al, MCR_DTR | MCR_RTS | MCR_OUT2
     out UART_MCR, al
 
+    ; initialize keyboard buffer
+    xor ax, ax
+    mov [KEYBUFLPTR], al
+    mov [KEYBUFRPTR], al
+
     ret
 
+;------------------------------------------------------------------------------
+; Initialize the interrupt vector table, needed to receive chars over UART
+;------------------------------------------------------------------------------
+init_ivt:
+    mov ax,0
+    mov es, ax
+    mov ax, isr_uart
+    mov word [es:0x60 * 4], ax          ; write offset
+    mov ax, 0x8000                      ; write segment
+    mov [es:0x60 * 4 + 2], ax
+    ret
+
+;------------------------------------------------------------------------------
 ; send a single character over the uart, character to be
 ; sent is set in AL; conserves AH
+;------------------------------------------------------------------------------
 putch:
     push ax
     call wait_tx_ready
@@ -159,48 +136,56 @@ putch:
     out UART_THR, al
     ret
 
+;------------------------------------------------------------------------------
 ; wait for TX to be ready
+;------------------------------------------------------------------------------
 wait_tx_ready:
     in al, UART_LSR     ; overwrites AL!
     test al, LSR_THRE
     jz wait_tx_ready
     ret
 
+;------------------------------------------------------------------------------
 ; wait for CTS to go low
+;------------------------------------------------------------------------------
 wait_cts:
     in al, UART_MSR     ; overwrites AL!
     test al, MSR_CTS
     jz wait_cts
     ret
 
-; interrupt service routine for my UART
+;------------------------------------------------------------------------------
+; interrupt service routine for the UART
+;------------------------------------------------------------------------------
 isr_uart:
     cli
     push ax
-
-    ; check whether this is reached or not
-    mov al,0x01
-    out 0x00, al
+    push bx
 
     ; Check why we got the interrupt (not strictly required for simple echo)
     in al, UART_IIR
-    and al, 0x06       ; Bits 1-2 hold reason
-    cmp al, 0x04       ; 0b100 = Received Data Available
+    and al, 0x06            ; Bits 1-2 hold reason
+    cmp al, 0x04            ; 0b100 = Received Data Available
     jne .done
 
-    ; Read received char
-    in al, UART_RBR
-
-    ; Echo it back
-    call putch
+    in al, UART_RBR         ; grab byte
+    mov si, KEYBUF          ; grab current keybuf position
+    xor bx,bx               ; reset BX
+    mov bl, [KEYBUFRPTR]    ; load offset
+    add si, bx              ; increment index
+    mov [si], al            ; store byte
+    inc bl                  ; increment pointer
+    mov [KEYBUFRPTR], bl    ; store pointer
 
 .done:
-    ; Send EOI to PIC if needed (not required if you're not using 8259)
+    pop bx
     pop ax
     sti
     iret
 
+;------------------------------------------------------------------------------
 ; Print 16-bit word in AX as 4 hex digits
+;------------------------------------------------------------------------------
 puthex16:
     push ax           ; Save AX
 
@@ -212,7 +197,9 @@ puthex16:
 
     ret
 
+;------------------------------------------------------------------------------
 ; write hex value
+;------------------------------------------------------------------------------
 puthex:
     push ax           ; Save AX since we will be modifying it
 
@@ -230,7 +217,9 @@ puthex:
     pop ax            ; Restore AX
     ret
 
+;------------------------------------------------------------------------------
 ; print newline character
+;------------------------------------------------------------------------------
 newline:
     mov al, CR
     call putch
@@ -238,22 +227,25 @@ newline:
     call putch
     ret
 
+;------------------------------------------------------------------------------
 ; Converts nibble in AL (0-15) to ASCII ('0'-'9', 'A'-'F')
+;------------------------------------------------------------------------------
 hexnibble:
     cmp al, 9
     jbe hex_is_digit
     add al, 7         ; Adjust for 'A'-'F'
 
+;------------------------------------------------------------------------------
+; Check whether value in AL is a digit
+;------------------------------------------------------------------------------
 hex_is_digit:
     add al, '0'       ; Convert to ASCII
     ret
 
-; === Pad to reset vector at 0xFFFF0
+;------------------------------------------------------------------------------
+; PADDING AND START VECTOR
+;------------------------------------------------------------------------------
 times 0xFFFF0 - 0x80000 - ($ - $$) db 0xFF
-
-; === Reset vector: placed at ROM offset 0x7FFF0
-    jmp 0x8000:0x0000     ; this jump hits address 0x80000
-
-; === Pad to 512 KiB total
+jmp 0x8000:0x0000                           ; this jump hits address 0x80000
 times 0x80000 - ($ - $$) db 0xFF
 
